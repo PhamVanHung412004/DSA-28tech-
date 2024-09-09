b7913fd606e5b2ec900ba69047ba456f2dd01d0f
PhamVanHung412004 <hp36780@gmail.com>
2024-09-09 21:17:14 +0700

    up:

---
+++ number_theory/a.py
@@ -0,0 +1,21 @@
+
+p1 = "D:/DSA/number_theory/bai_toan_ve_snt/"
+p2 = "D:/DSA/number_theory/phan_tich_thua_so_nguyen_to/"
+p3 = "D:/DSA/number_theory/phep_toan_modulo/"
+p4 = "D:/DSA/number_theory/tong_hop/"
+for i in range(1,14):
+    with open(p1 + "bai" + str(i) + ".cpp","w") as file:
+        file.write("")
+    
+
+for i in range(1,11):
+    with open(p2 + "bai" + str(i) + ".cpp","w") as file:
+        file.write("")
+
+for i in range(1,41):
+    with open(p4 + "bai" + str(i) + ".cpp","w") as file:
+        file.write("")
+    
+for i in range(1,13):
+    with open(p3 + "bai" + str(i) + ".cpp","w") as file:
+        file.write("")
